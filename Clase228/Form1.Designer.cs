658c8df0cd201a86347669b763871bb7bd86e281
Pablo Exequiel Gutierrez <pablo.e.gutierrez@outlook.com>
2023-08-24 00:34:22 -0300

    Agregar archivos de proyecto.

---
+++ Clase228/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Clase228
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.btLongitud = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btPrueba = new System.Windows.Forms.Button();
+            this.BtMayuscula = new System.Windows.Forms.Button();
+            this.btMinuscula = new System.Windows.Forms.Button();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.txtBuscado = new System.Windows.Forms.TextBox();
+            this.txtDeletrear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFrase.Location = new System.Drawing.Point(56, 31);
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(291, 38);
+            this.txtFrase.TabIndex = 0;
+            // 
+            // btLongitud
+            // 
+            this.btLongitud.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLongitud.Location = new System.Drawing.Point(56, 75);
+            this.btLongitud.Name = "btLongitud";
+            this.btLongitud.Size = new System.Drawing.Size(144, 39);
+            this.btLongitud.TabIndex = 1;
+            this.btLongitud.Text = "Longitud";
+            this.btLongitud.UseVisualStyleBackColor = true;
+            this.btLongitud.Click += new System.EventHandler(this.btLongitud_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(50, 117);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 31);
+            this.lblResultado.TabIndex = 2;
+            // 
+            // btPrueba
+            // 
+            this.btPrueba.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPrueba.Location = new System.Drawing.Point(209, 75);
+            this.btPrueba.Name = "btPrueba";
+            this.btPrueba.Size = new System.Drawing.Size(138, 39);
+            this.btPrueba.TabIndex = 3;
+            this.btPrueba.Text = "Prueba";
+            this.btPrueba.UseVisualStyleBackColor = true;
+            this.btPrueba.Click += new System.EventHandler(this.btPrueba_Click);
+            // 
+            // BtMayuscula
+            // 
+            this.BtMayuscula.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtMayuscula.Location = new System.Drawing.Point(353, 75);
+            this.BtMayuscula.Name = "BtMayuscula";
+            this.BtMayuscula.Size = new System.Drawing.Size(196, 36);
+            this.BtMayuscula.TabIndex = 4;
+            this.BtMayuscula.Text = "MAYUSCULA";
+            this.BtMayuscula.UseVisualStyleBackColor = true;
+            this.BtMayuscula.Click += new System.EventHandler(this.BtMayuscula_Click);
+            // 
+            // btMinuscula
+            // 
+            this.btMinuscula.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMinuscula.Location = new System.Drawing.Point(555, 76);
+            this.btMinuscula.Name = "btMinuscula";
+            this.btMinuscula.Size = new System.Drawing.Size(196, 36);
+            this.btMinuscula.TabIndex = 5;
+            this.btMinuscula.Text = "minuscula";
+            this.btMinuscula.UseVisualStyleBackColor = true;
+            this.btMinuscula.Click += new System.EventHandler(this.btMinuscula_Click);
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBuscar.Location = new System.Drawing.Point(757, 76);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(196, 36);
+            this.btBuscar.TabIndex = 6;
+            this.btBuscar.Text = "BUSCAR";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // txtBuscado
+            // 
+            this.txtBuscado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscado.Location = new System.Drawing.Point(386, 32);
+            this.txtBuscado.Name = "txtBuscado";
+            this.txtBuscado.Size = new System.Drawing.Size(291, 38);
+            this.txtBuscado.TabIndex = 7;
+            // 
+            // txtDeletrear
+            // 
+            this.txtDeletrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDeletrear.Location = new System.Drawing.Point(959, 76);
+            this.txtDeletrear.Name = "txtDeletrear";
+            this.txtDeletrear.Size = new System.Drawing.Size(196, 36);
+            this.txtDeletrear.TabIndex = 8;
+            this.txtDeletrear.Text = "Deletrear";
+            this.txtDeletrear.UseVisualStyleBackColor = true;
+            this.txtDeletrear.Click += new System.EventHandler(this.txtDeletrear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 450);
+            this.Controls.Add(this.txtDeletrear);
+            this.Controls.Add(this.txtBuscado);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.btMinuscula);
+            this.Controls.Add(this.BtMayuscula);
+            this.Controls.Add(this.btPrueba);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btLongitud);
+            this.Controls.Add(this.txtFrase);
+            this.Name = "Form1";
+            this.Text = "Minuscula";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.Button btLongitud;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btPrueba;
+        private System.Windows.Forms.Button BtMayuscula;
+        private System.Windows.Forms.Button btMinuscula;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.TextBox txtBuscado;
+        private System.Windows.Forms.Button txtDeletrear;
+    }
+}
+
---
+++ Clase228/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase228
+{
+    public partial class Form1 : Form
+    {
+        private int i;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btLongitud_Click(object sender, EventArgs e)
+        {
+            int CantCaracteres = 0;
+            CantCaracteres = txtFrase.Text.Length;
+
+            lblResultado.Text = CantCaracteres.ToString();
+        }
+
+        private void btPrueba_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "La Cantidad de Caracteres de Pepe es ".Length.ToString();
+        }
+
+        private void BtMayuscula_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = txtFrase.Text.ToUpper();
+        }
+
+        private void btMinuscula_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = txtFrase.Text.ToLower();
+
+        }
+
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+            if (txtBuscado.Text.Contains(txtBuscado.Text))
+            {
+                lblResultado.Text = "SI";
+            }
+            else
+            {
+                lblResultado.Text = "NO";
+            }
+                }
+
+        private void txtDeletrear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < txtFrase.Text.Length; i++) ;
+            {
+                lblResultado.Text += txtFrase.Text.Substring(i,1) + "\r\n";
+            }
+        }
+    }
+    }
+
